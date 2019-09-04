8af18f669ef9c4ce59c41e5ed12d454ef9200e71
Michele Loria <info@micheleloria.net>
2019-09-04 16:50:20 +0200

    Scroll to top on route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import {BrowserRouter as Router, Route, withRouter} from 'react-router-dom';
 import './App.css';
 
 import {Header} from './components/Header';
 import {Home, Services, Preview, Contacts, Project, Projects} from './pages';
 
+const ScrollToTop = withRouter(({children, location: {pathname}}) => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return children || null;
+});
+
 function App() {
   return (
     <Router>
-      <Header />
-      <Route path="/" exact component={Home} />
-      <Route path="/projects" exact component={Projects} />
-      <Route path="/projects/:type" exact component={Projects} />
-      <Route path="/projects/:type/:year/:slug" component={Project} />
-      <Route path="/preview/" component={Preview} />
-      <Route path="/services/" component={Services} />
-      <Route path="/contacts/" component={Contacts} />
+      <ScrollToTop>
+        <Header />
+        <Route path="/" exact component={Home} />
+        <Route path="/projects" exact component={Projects} />
+        <Route path="/projects/:type" exact component={Projects} />
+        <Route path="/projects/:type/:year/:slug" component={Project} />
+        <Route path="/preview/" component={Preview} />
+        <Route path="/services/" component={Services} />
+        <Route path="/contacts/" component={Contacts} />
+      </ScrollToTop>
     </Router>
   );
 }
