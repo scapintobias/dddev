4a13292d52dbbbd306e3fa6d9101f1b576fa32c4
scapintobias <scapintobias@gmail.com>
2019-11-18 19:41:08 +0100

    Grid

--- src/components/Work/component.jsx
+++ src/components/Work/component.jsx
@@ -1,15 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CN from "classnames";
 import "../../tachyons.css";
-
+import "./style.css";
 export const Grid = ({ children }) => <div className="grid">{children}</div>;
 
-export const Cell = ({ title, subtitle, description }) => (
-  <div className="flex justify-center pv4 items-center item item-b flex justify-center pv4 items-center hover-white bg-animate hover-bg-green">
-    <div className="w-80 tc">
-      <p className="b ttu f6 sans-serif">{title}</p>
-      <h1 className="helvetica b lh-leading mv0 pv3">{subtitle}</h1>
-      <p className="caslon lh-solid f3 i">{description}</p>
+export const Cell = ({
+  link,
+  title,
+  subtitle,
+  description,
+  text,
+  className
+}) => (
+  <Link to={link}>
+    <div className={CN("flex justify-center items-center item", className)}>
+      <div className="w-80 tc">
+        <p className="b ttu f6 helvetica">{title}</p>
+        <h1 className="helvetica b lh-leading pv3">{subtitle}</h1>
+        <p className="pb4 pt3 f3 i caslon">{description}</p>
+        <p className="caslon w-90 center lh-solid f3">{text}</p>
+      </div>
     </div>
-  </div>
+  </Link>
 );
--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -8,9 +8,57 @@ export function Tab() {
   return (
     <Grid>
       <Cell
+        link="./"
         title="UX"
         subtitle="Metro de Lima"
         description="Wayfinding design"
+        className="hover-white bg-animate hover-bg-green item-b pv4"
+      />
+
+      <Cell
+        link="./"
+        title="UX"
+        subtitle="Metro de Lima"
+        description="Wayfinding design"
+        text="«La narrativa deve illuminaire, esplodere, ristorare. Non c'è alcuna filosofia morale nella narrativa oltre all'eccellenza. L'intensità della sensibilità e la rapidità le ho sempre ritenute. La gente cerca sempra la morale perché confonde narrative e filosofia»"
+        className="hover-white bg-animate  hover-bg-blue item-c pv5"
+      />
+      <clearfix />
+      <Cell
+        link="./"
+        title="UX"
+        subtitle="Metro de Lima"
+        description="Wayfinding design"
+        className="hover-white bg-animate hover-bg-blue item-d pv4"
+      />
+      <Cell
+        link="./"
+        title="UX"
+        subtitle="Metro de Lima"
+        description="Wayfinding design"
+        className="hover-white bg-animate hover-bg-blue item-e pv4"
+      />
+
+      <Cell
+        link="./"
+        title="UX"
+        subtitle="Metro de Lima"
+        description="Wayfinding design"
+        className="hover-white bg-animate hover-bg-blue item-f pv4"
+      />
+      <Cell
+        link="./"
+        title="UX"
+        subtitle="Metro de Lima"
+        description="Wayfinding design"
+        className="hover-white bg-animate hover-bg-blue item-g pv4"
+      />
+      <Cell
+        link="./"
+        title="UX"
+        subtitle="Metro de Lima"
+        description="Wayfinding design"
+        className="hover-white bg-animate hover-bg-blue item-h pv4"
       />
     </Grid>
   );
