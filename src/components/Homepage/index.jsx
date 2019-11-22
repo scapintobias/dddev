7e4f9257d45ff1870d8dbe2e677ebaf9fc8c9fb3
scapintobias <scapintobias@gmail.com>
2019-11-22 02:41:12 +0100

    footer

--- src/components/Homepage/index.jsx
+++ src/components/Homepage/index.jsx
@@ -6,22 +6,23 @@ const Random = () => (
   <span>
   based in{' '}
 <TextLoop interval={550}>
-        <span>Italy</span>
         <span>Germany</span>
         <span>Australia</span>
-        <span>United Kingdom</span>
-        <span>New Zealand</span>
-        <span>United States</span>
+        <span>UK</span>
+        <span>Maldives</span>
+        <span>USA</span>
+        <span>Italy</span>
         <span>Scotland</span>
         <span>Canada</span>
         <span>Bali</span>
+        <span>Ireland</span>
       </TextLoop>.
   </span>
 );
 
 export const Homepage = props => (
   <main>
-    <section className="pt2 pl3 pr3 pl2-m pr1-m">
+    <section className="pt2 pl3 pr3 pl2-m pr2-m pb4">
       <p className="helvetica lh-title tracked-tight f-7 f-45-m fw6">
         <span className="orange">Product</span> &{' '}
         <span className="orange">Interaction</span> Designer <Random />
@@ -33,13 +34,16 @@ export const Homepage = props => (
         powerful typography and elegant design. Please feel free to check out my{' '}
         <span className="helvetica fw7 tracked-tight tracked-tight-m link black hover-orange">
           <Link to="/case-studies">case studies</Link>
-        </span>
-        . Whether interested in reading more{' '}
+        </span>.</p>
+        <p className="pt2 pt3-m caslon f-6 f2-m lh-title-s">Whether interested in reading more{' '}
         <b className="helvetica fw7 tracked-tight tracked-tight-m link black hover-dark-blue">
           <a href="/about">about me</a>
         </b>
-        , sit back: it could get quite long.
+        , sit back: it gets quite long.
       </p>
+    </section>
+    <section className="pl3 pr5 pb5 caslon silver f4">
+    ©DisciplineDesign 2019
     </section>
   </main>
 );
