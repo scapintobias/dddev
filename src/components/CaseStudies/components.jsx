5a006609774f52f2ff5de642ae7d1c3e2ff19a19
scapintobias <scapintobias@gmail.com>
2019-11-23 14:22:39 +0100

    Refinements

--- src/components/CaseStudies/components.jsx
+++ src/components/CaseStudies/components.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export const Raster = ( { children } ) => (
-	<div className="justify-center flex flex-column w-100-m w-90-l ml3-m pr3-m pb5 min-vh-100">
+	<div className="justify-center flex flex-column ml3-m pr3-m pb5 min-vh-100">
 		<article className="sans-serif  dark-gray center">
 			{children}
 		</article>
@@ -9,32 +9,32 @@ export const Raster = ( { children } ) => (
 );
 
 export const Date = ( { place, time } ) => (
-	<section className="mw68 helvetica mauto pb4 pb2-m pt3 pt2-m">
-		<div className="fw6 f5 lh-solid">
-			<p>{place}</p>
-			<time className="ttu silver">{time}</time>
+	<section className="mw68 w-100-l helvetica mauto pb4 pb2-m pt3 pt2-m">
+		<div className="fw6 f4">
+			<p>{place}<br></br>
+				<time className="ttu silver">{time}</time></p>
 		</div>
 	</section>
 );
 
 export const Title = ( { title, subtitle, description, line2 } ) => (
-	<header className="mw68 helvetica lh-solid bb bw4 mauto justify-center b--dark-blue mb5 mr3-m">
+	<header className="mw68 w-100-m w-100-l   mb3 mr3-m mauto justify-center helvetica lh-solid bb bw4 b--dark-blue">
 		<p className=" f-6 f-5-m fw7 tracked-tight dark-blue pb2-m pb6">{title}</p>
 		<p className="fw6 f1 green pb3 pb2-m fw6-m f2-m tracked">{subtitle}</p>
-		<p className="f2 minion fw5 fw6-m f25-m mb0 w-70 w-100-m light-red pb4 pb4-m lh-solid">{description}</p>
-		<p className="f2 minion fw5 fw6-m f25-m mb0 w-70 w-100-m light-red pb2 pb2-m lh-solid">{line2}</p>
+		<p className="f2 minion fw5 fw6-m f25-m mb0 w-80 w-100-m light-red pb4 pb4-m lh-solid">{description}</p>
+		<p className="f2 minion fw5 fw6-m f25-m mb0 w-80 w-100-m light-red pb4 pb2-m lh-solid">{line2}</p>
 	</header>
 );
 
 export const Par = ( { txt } ) => (
 
-	<section className="mw68 mauto w-100-m w-90-l">
+	<section className="mw68 mauto">
 		<p className="minion fw5 abc pr3">{txt}</p></section>
 );
 
 export const ParImg = ( { oneimg } ) => (
 
-	<div className="mw68 w-100-m w-90-l mauto pt4 pb4 pr3-m">
+	<div className="mw68 mauto pt4 pb4">
 		<img src={oneimg} alt="img"></img>
 	</div>
 );
@@ -42,13 +42,13 @@ export const ParImg = ( { oneimg } ) => (
 
 export const SinImg = ( { oneimg } ) => (
 
-	<div className="mw800 w-100-m w-90-l mauto pt4 pb4 pr3-m">
+	<div className="mw800 mauto pt4 pb4">
 		<img src={oneimg} alt="img"></img>
 	</div>
 );
 export const ThreeImg = ( { image1, image2, image3 } ) => (
 
-	<div className="w-90 w-100-m w-90-l flex flex-row justify-center items-center-m flex-column-m mauto pt4 pb4 pr3-m">
+	<div className="w-100 flex flex-row justify-center items-center-m items-center-l flex-column-l flex-column-m mauto pt4 pb4">
 		<img src={image1} alt="img" className="pa2"></img>
 		<img src={image2} alt="img" className="pa2"></img>
 		<img src={image3} alt="img" className="pa2"></img>
@@ -58,7 +58,7 @@ export const ThreeImg = ( { image1, image2, image3 } ) => (
 
 export const TwoImg = ( { image1, image2 } ) => (
 
-	<div className="w-90 w-100-m w-90-l flex flex-row justify-center items-center-m flex-column-m mauto pt4 pb4 pr3-m">
+	<div className="w-100 flex flex-row justify-center items-center-m flex-column-m mauto pt4 pb4">
 		<img src={image1} alt="img" className="pa2"></img>
 		<img src={image2} alt="img" className="pa2"></img>
 	</div>
--- src/components/Work/index.jsx
+++ src/components/Work/index.jsx
@@ -1,49 +1,49 @@
 import React from 'react';
-import {Footer} from '../footer.jsx';
-import {Grid} from './grid.jsx';
-import {Cell} from './cell.jsx';
-import {CellText} from './cell.jsx';
-import {CellF} from './cell.jsx';
+import { Footer } from '../footer.jsx';
+import { Grid } from './grid.jsx';
+import { Cell } from './cell.jsx';
+import { CellText } from './cell.jsx';
+import { CellF } from './cell.jsx';
 
 export function Tab() {
   return (
-     <main className="min-vh-100">
-    <Grid>
-      <CellText
-        link="./"
-        title="UX"
-        subtitle="Metro de Lima"
-        description="Wayfinding design"
-        text="«La narrativa deve illuminaire, esplodere, ristorare. Non c'è alcuna filosofia morale nella narrativa»"
-        className="hover-white bg-animate b--silver hover-bg-green br-l bb-n-l bt bl bb br-m fl w-50 w-100-m w-100-l h8 pv5"
-      />
+    <main className="min-vh-100">
+      <Grid>
+        <CellText
+          link="./"
+          title="UX"
+          subtitle="Metro de Lima"
+          description="Wayfinding design"
+          text="«La narrativa deve illuminaire, esplodere, ristorare. Non c'è alcuna filosofia morale nella narrativa»"
+          className="hover-white bg-animate b--silver hover-bg-green br-l bb-n-l bt bl bb br-m fl w-50 w-100-m w-100-l h8 pv5"
+        />
 
-      <CellText
-        link="/case-studies/eNovia"
-        title="UX"
-        subtitle="e-Novia"
-        description="A sourceful experience"
-        text="Where I learned to do research, to fail, to work as a team member, and when to shut up."
-        className="hover-dark-blue bg-animate hover-bg-light-blue bl br bt bt-n-m bb b--silver fl w-50 w-100-m w-100-l pv5"
-      />
+        <CellText
+          link="/case-studies/eNovia"
+          title="UX"
+          subtitle="e-Novia"
+          description="A sourceful experience"
+          text="Where I learned to do research, to fail, to work as a team member, and—more importantly—when to shut up."
+          className="hover-dark-blue bg-animate hover-bg-light-blue bl br bt bt-n-m bb b--silver fl w-50 w-100-m w-100-l pv5"
+        />
 
-      <CellF
-        link="./"
-        title="UX"
-        subtitle="Trenitalia"
-        description="Ticket totem redesign"
-        className="hover-white bg-animate hover-bg-dark-blue bl bb b--silver bl br-m bb br-l mb0-l mb0-m fl w-25 w-100-m w-100-l pv5 mb1"
-      />
-      <Cell
-        link="./"
-        title="REACT"
-        subtitle="DisciplineDesign"
-        description="How I built this website"
-        className="hover-orange bg-animate hover-bg-black-10 bb bl br b--silver fl w-75 w-100-m w-100-l pv5 mb1 mb3-m mb3-l"
-      />
+        <CellF
+          link="./"
+          title="UX"
+          subtitle="Trenitalia"
+          description="Ticket totem redesign"
+          className="hover-white bg-animate hover-bg-dark-blue bl bb b--silver bl br-m bb br-l mb0-l mb0-m fl w-25 w-100-m w-100-l pv5 mb1"
+        />
+        <Cell
+          link="./"
+          title="REACT"
+          subtitle="DisciplineDesign"
+          description="How I built this website"
+          className="hover-orange bg-animate hover-bg-black-10 bb bl br b--silver fl w-75 w-100-m w-100-l pv5 mb1 mb3-m mb3-l"
+        />
 
-    </Grid>
-    <Footer />
+      </Grid>
+      <Footer />
     </main>
   );
 }
