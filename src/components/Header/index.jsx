be692e9cebb315f4fabe7e3e950ec2ee3f42942d
scapintobias <scapintobias@gmail.com>
2019-12-10 14:33:51 +0100

    First release - iteration 1

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -16,46 +16,51 @@ export function Header(props) {
 	<div className="menu-icon" onClick={toggleExpanded}>
 		<span className="navicon"/>
 	</div>
-	<ul className="fw5 fw4-m menu">
-		<li className="f4 pt3-m tr-m tr-l pr3-m">
+	<ul className="fw5 menu">
+		<li className="f4 pt3-m tr-m tr-l pr35-m pt3-l pr35-l">
 			<NavLink exact to="/case-studies">
 				Case Studies
 			</NavLink>
 		</li>
-		<li className="f4 pt3-m tr-m tr-l pr3-m">
+		<li className="f4 pt3-m tr-m tr-l pr35-m pt3-l pr35-l">
 			<NavLink exact to="/about">About</NavLink>
 		</li>
 
-		<li className="pt3-m pb2-m tr-m tr-l pr3-m">
+		<li className="pt3-m pb2-m tr-m tr-l pr35-m pt3-m pr3-m pt3-l pr35-l">
 			<a href="mailto:info@discipline.design?subject=Enquiry">
-				<span className="helvetica  f3-m f4 fw4 fw5-m">@</span>
+				<span className="helvetica f4">@</span>
 			</a>
 		</li>
-		<li className="f3-m pt2-m tr-m tr-l pr3-m">
-			<a href="https://www.instagram.com/discipline.design" target="_blank" rel="noopener noreferrer">
+		<li className="f3-m pt2-m tr-m tr-l pr35-m mt05 pt3-l pr35-l">
+			<a id="insta" href="https://www.instagram.com/discipline.design" target="_blank" rel="noopener noreferrer">
 				<svg
 					x="0px"
 					y="0px"
-					width="24px"
-					height="24px"
-					viewBox="0 0 30 30"
-					enableBackground="new 0 0 30 30">
+					width="19px"
+					height="19px"
+					viewBox="0 0 20 20"
+					enableBackground="new 0 0 20 20"
+					bottom="4"
+					>
 					<path
 						id="instagram"
 						fill="#001B44"
-						d="M22.107,3.415H7.893c-2.469,0-4.479,2.007-4.479,4.477v4.73v9.486c0,2.469,2.01,4.479,4.479,4.479h14.215
-							c2.469,0,4.479-2.01,4.479-4.479v-9.486v-4.73C26.586,5.421,24.576,3.415,22.107,3.415 M23.393,6.086l0.512-0.004v0.511v3.416
-							l-3.916,0.014l-0.012-3.928L23.393,6.086z M11.693,12.622c0.742-1.028,1.945-1.7,3.307-1.7s2.564,0.672,3.307,1.7
-							c0.484,0.67,0.771,1.49,0.771,2.379c0,2.248-1.828,4.078-4.078,4.078c-2.248,0-4.078-1.83-4.078-4.078
-							C10.922,14.112,11.211,13.292,11.693,12.622 M24.328,22.107c0,1.225-0.994,2.219-2.221,2.219H7.893
-							c-1.225,0-2.219-0.994-2.219-2.219v-9.486h3.459C8.832,13.356,8.664,14.159,8.664,15c0,3.494,2.842,6.335,6.336,6.335
-							s6.336-2.842,6.336-6.335c0-0.842-0.17-1.645-0.467-2.379h3.459V22.107z"/>
+						d="M14,0H6C2.7,0,0,2.7,0,6V14c0,3.3,2.7,6,6,6H14c3.3,0,6-2.7,6-6V6C20,2.7,17.3,0,14,0z M18,14c0,2.2-1.8,3.9-3.9,3.9H6
+		C3.8,18,2,16.2,2,14V6C2,3.8,3.8,2,6,2H14C16.2,2,18,3.8,18,6V14L18,14z"/><path
+		id="instagram"
+		fill="#001B44"
+		d="M10,4.8c-2.9,0-5.2,2.3-5.2,5.2s2.3,5.2,5.2,5.2s5.2-2.3,5.2-5.2S12.9,4.8,10,4.8z M10,13.2c-1.7,0-3.2-1.4-3.2-3.2
+		S8.3,6.8,10,6.8c1.7,0,3.2,1.4,3.2,3.2C13.2,11.7,11.7,13.2,10,13.2z"/>
+	<circle
+		id="instagram"
+		fill="#001B44"
+		 cx="15.2" cy="4.9" r="1.2"/>
 				</svg>
 			</a>
 		</li>
-		<li className="pb3-m pt3-m tr-m tr-l pr3-m">
+		<li className="pb3-m pb3-l pt3-m tr-m tr-l pr35-m pt3-l pr35-l">
 			<a href="https://www.linkedin.com/in/scapintobias" target="_blank" rel="noopener noreferrer">
-				<span className="futurab f3 pb1 f2-m tracked">in</span>
+				<span className="futurab f3 pb1 tracked">in</span>
 			</a>
 		</li>
 	</ul> < /header> < div className = "header-clearfix" / > < />
