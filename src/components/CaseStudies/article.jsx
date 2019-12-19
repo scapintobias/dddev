8e4650b0995ccbe3fb096cc726a30564025a6ec1
scapintobias <scapintobias@gmail.com>
2019-12-19 11:32:57 +0100

    Second release - second iteration

--- src/components/CaseStudies/article.jsx
+++ src/components/CaseStudies/article.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 export const Par = ({title, subtitle, image, children}) => (
-	<section className="ma3 pb3 flex flex-row flex-column-m">
+	<section className="mauto ma3-m pv3 pv2-m mw9 flex flex-row flex-column-m">
 
-      <div className="helvetica navy pr3 f2 f35-t w-third  w-100-m fw6 tracked lh-solid">{title}
-      <div className="minion orange f2 f35-t fw2 i pr3 pb3-l pb3-t pb3-m lh-solid">{subtitle}</div>
-		<img src={image} className="w-100"/>
-      </div>
+      <div className="helvetica navy ph3 ph0-m f2 f35-t tr tl-l tl-t tl-m w-40 w-30-l w-30-t w-100-m fw6 tracked lh-solid lh-solid-m">{title}
+      <div className="minion orange f2 f3-t fw2 i pr3 pb3-l pb3-t pb0-m lh-solid lh-solid-m">{subtitle}</div>
+		<img src={image} className="w-100"/></div>
 
 
-		<div className="minion navy pr3 fw5 w-two-thirds w-100-m abc">{children}</div>
+
+		<div className="minion navy pr3 pr0-m fw5 w-50 w-80-l w-70-t w-100-m abc">{children}</div>
 	</section>
 );
 
