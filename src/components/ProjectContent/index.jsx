b4566cdf4199c4d746642427e30fc774d8a3e7e8
scapintobias <scapintobias@gmail.com>
2020-01-26 11:05:06 +0100

    New function createContent rev2

--- src/components/ProjectContent/index.jsx
+++ src/components/ProjectContent/index.jsx
@@ -1,8 +1,6 @@
-import React from "react";
 
 export function ProjectContent( { project } ) {
 	const {/* title, subtitle, type, year,*/ content } = project;
-	const { } = content;
 	console.log( project );
 	return (
 		content
